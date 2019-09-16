3e5cafff1c254202c81c2f11b9b1826ce2c0f15f
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-09-16 18:00:48 -0500

    3 tests pass

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+import React from 'react';
+import { render } from 'react-testing-library';
+import Controls from '../controls/Controls';
+
+
+it('shows controls', () => {
+    const { queryByText } = render(<Controls />);
+
+    expect(queryByText(
+        (/lock gate/i)&&(/close gate/i) && (/unlock gate/i)&&(/close gate/i)
+        ))
+    expect(queryByText(/close gate/i))
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,12 @@
 // Test away!
+import React from 'react';
+import { render } from 'react-testing-library';
+import Display from './Display';
+
+it('defaults to unlocked and open',  () => {
+    const { queryByText }  = render(< Display />);
+
+    expect(queryByText(/unlocked/i));
+    expect(queryByText(/open/i));
+})
+
