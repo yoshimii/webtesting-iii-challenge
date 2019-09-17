1bf1763298cc5fa95b20b33c700053f25d8a5e49
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-09-16 23:41:46 -0500

    test if gate is open or closed when locked and unlocked

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -8,7 +8,7 @@ it('shows controls', () => {
     const { queryByText } = render(<Controls />);
 
     expect(queryByText(
-        (/lock gate/i)&&(/close gate/i) && (/unlock gate/i)&&(/close gate/i)
+        ((/lock gate/i)&&(/close gate/i)) && ((/unlock gate/i)&&(/open gate/i))
         ))
     expect(queryByText(/close gate/i))
 })
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -10,3 +10,29 @@ it('defaults to unlocked and open',  () => {
     expect(queryByText(/open/i));
 })
 
+it('displays if gate is closed and if it is locked', () => {
+    const state = {
+        locked: true,
+        closed: true,
+      };
+
+      const { queryByText } = render(<Display locked={state.locked} closed={state.closed}/>);
+
+      expect(queryByText(/closed/i))
+
+})
+
+it('displays if gate is open/closed and if it is locked/unlocked', () => {
+    const state = {
+        locked: false,
+        closed: false,
+      };
+
+      const { queryByText } = render(<Display locked={state.locked} closed={state.closed}/>);
+
+      expect(queryByText(/open/i))
+
+})
+
+
+
